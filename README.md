8b75c6405e279a054672bf41dbfb291cde01a18c
0x7d8 <rjansengd@gmail.com>
2024-07-20 20:20:54 +0200

    initial files

---
+++ README.md
@@ -0,0 +1,23 @@
+# pterodactyl-yolks - MCJars Java Docker Images
+
+pterodactyl-yolks is a collection of Docker images for use with Pterodactyl Panel. These images are built on top of the official Docker images for Java, but with a few modifications to make them more suitable for use with the Official [Version Changer for Pterodactyl](https://www.sourcexchange.net/products/version-changer) addon.
+
+## Available Images
+
+* [`java`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java)
+  * [`java8`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/8)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_8`
+  * [`java11`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/11)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_11`
+  * [`java16`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/16)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_16`
+  * [`java17`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/17)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_17`
+  * [`java18`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/18)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_18`
+  * [`java19`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/19)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_19`
+  * [`java21`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/21)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_21`
+  * [`java22`](https://github.com/mcjars/pterodactyl-yolks/tree/master/java/22)
+    * `ghcr.io/mcjars/pterodactyl-yolks:java_22`
---
+++ java/entrypoint.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2021 Matthew Penner
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Print Java version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+java -version
+
+if [[ "$OVERRIDE_STARTUP" == "1" ]]; then
+	FLAGS=()
+
+	if [[ "$SIMD_OPERATIONS" == "1" ]]; then
+		FLAGS+=("--add-modules=jdk.incubator.vector")
+	fi
+
+	if [[ "$FLAGS" == "Aikar's Flags" ]]; then
+		FLAGS+=("-XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true")
+	elif [[ "$FLAGS" == "Velocity" ]]; then
+		FLAGS+=("-XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15")
+	fi
+
+	SERVER_MEMORY_REAL=(($SERVER_MEMORY*($MAXIMUM_RAM/100)))
+	PARSED="java ${FLAGS[*]} -Xms${SERVER_MEMORY} -Xmx${SERVER_MEMORY} ${JAVA_OPTS} -jar ${SERVER_JARFILE}"
+
+	# Display the command we're running in the output, and then execute it with the env
+	# from the container itself.
+	printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+	# shellcheck disable=SC2086
+	exec env ${PARSED}
+else
+	# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+	# variable format of "${VARIABLE}" before evaluating the string and automatically
+	# replacing the values.
+	PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+	# Display the command we're running in the output, and then execute it with the env
+	# from the container itself.
+	printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+	# shellcheck disable=SC2086
+	exec env ${PARSED}
+fi
