69119904229379244eea314eb201465a6779b140
0x7d8 <rjansengd@gmail.com>
2024-07-20 21:25:43 +0200

    fix typo

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -77,7 +77,7 @@ if [[ "$AUTOMATIC_UPDATING" == "1" ]]; then
 			if [ "$(echo $API_RESPONSE | jq -r '.build.id')" != "$(echo $API_RESPONSE | jq -r '.latest.id')" ]; then
 				echo -e "\033[1m\033[33mcontainer@pterodactyl~ \033[0mNew build found. Updating server..."
 
-				$BUILD_ID=$(echo $API_RESPONSE | jq -r '.build.id')
+				$BUILD_ID="$(echo $API_RESPONSE | jq -r '.build.id')"
 				bash <(curl -s "https://versions.mcjars.app/api/v1/script/$BUILD_ID/bash?echo=false")
 
 				echo -e "\033[1m\033[33mcontainer@pterodactyl~ \033[0mServer has been updated"
