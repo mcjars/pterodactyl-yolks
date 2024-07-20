13f19792ec9f707810aa4d73a13907b4abf28566
0x7d8 <rjansengd@gmail.com>
2024-07-20 21:44:15 +0200

    fix var name

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -98,9 +98,9 @@ if [[ "$OVERRIDE_STARTUP" == "1" ]]; then
 		FLAGS+=("--add-modules=jdk.incubator.vector")
 	fi
 
-	if [[ "$FLAGS" == "Aikar Flags" ]]; then
+	if [[ "$ADDITIONAL_FLAGS" == "Aikar's Flags" ]]; then
 		FLAGS+=("-XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=8M -XX:G1HeapWastePercent=5 -XX:G1MaxNewSizePercent=40 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1NewSizePercent=30 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -XX:MaxGCPauseMillis=200 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true")
-	elif [[ "$FLAGS" == "Velocity Flags" ]]; then
+	elif [[ "$ADDITIONAL_FLAGS" == "Velocity Flags" ]]; then
 		FLAGS+=("-XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15")
 	fi
 
