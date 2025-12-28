cd0d5cc2185320061efee360db0248f51a44d2d8
iRazz <hi@irazz.lol>
2025-12-28 22:39:07 +0200

    im blind

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -243,19 +243,19 @@ if [[ "$OVERRIDE_STARTUP" == "1" ]]; then
 		FLAGS+=("-XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15")
 	fi
 
-	if [[ "$MINEHUT_SUPPORT" == "Velocity" ]]; then
+	if [[ "$MINEKEEP_SUPPORT" == "Velocity" ]]; then
 		FLAGS+=("-Dmojang.sessionserver=https://sessionserver.minekeep.net/session/minecraft/hasJoined")
-	elif [[ "$MINEHUT_SUPPORT" == "Waterfall" ]]; then
+	elif [[ "$MINEKEEP_SUPPORT" == "Waterfall" ]]; then
 		FLAGS+=("-Dwaterfall.auth.url=\"https://sessionserver.minekeep.net/session/minecraft/hasJoined?username=%s&serverId=%s%s\")")
-	elif [[ "$MINEHUT_SUPPORT" = "Bukkit" ]]; then
+	elif [[ "$MINEKEEP_SUPPORT" = "Bukkit" ]]; then
 		FLAGS+=("-Dminecraft.api.auth.host=https://authserver.mojang.com/ -Dminecraft.api.account.host=https://api.mojang.com/ -Dminecraft.api.services.host=https://api.minecraftservices.com/ -Dminecraft.api.profiles.host=https://api.mojang.com/ -Dminecraft.api.session.host=https://sessionserver.minekeep.net")
 	fi
 
-	if [[ "$MINEKEEP_SUPPORT" == "Velocity" ]]; then
+	if [[ "$MINEHUT_SUPPORT" == "Velocity" ]]; then
 		FLAGS+=("-Dmojang.sessionserver=https://api.minehut.com/mitm/proxy/session/minecraft/hasJoined")
-	elif [[ "$MINEKEEP_SUPPORT" == "Waterfall" ]]; then
+	elif [[ "$MINEHUT_SUPPORT" == "Waterfall" ]]; then
 		FLAGS+=("-Dwaterfall.auth.url=\"https://api.minehut.com/mitm/proxy/session/minecraft/hasJoined?username=%s&serverId=%s%s\")")
-	elif [[ "$MINEKEEP_SUPPORT" = "Bukkit" ]]; then
+	elif [[ "$MINEHUT_SUPPORT" = "Bukkit" ]]; then
 		FLAGS+=("-Dminecraft.api.auth.host=https://authserver.mojang.com/ -Dminecraft.api.account.host=https://api.mojang.com/ -Dminecraft.api.services.host=https://api.minecraftservices.com/ -Dminecraft.api.session.host=https://api.minehut.com/mitm/proxy")
 	fi
 
